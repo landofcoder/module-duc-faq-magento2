db32feb333f10e742bc1603951cdd11809680603
ducpa23 <ducpa233@gmail.com>
2020-03-12 17:04:17 +0700

    create_faq_v2

---
+++ Setup/UpgradeSchema.php
@@ -0,0 +1,30 @@
+<?php
+
+
+namespace Ducpa23\FAQ\Setup;
+
+use Magento\Framework\Setup\UpgradeSchemaInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\SchemaSetupInterface;
+
+/**
+ * Class UpgradeSchema
+ *
+ * @package Ducpa23\FAQ\Setup
+ */
+class UpgradeSchema implements UpgradeSchemaInterface
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    public function upgrade(
+        SchemaSetupInterface $setup,
+        ModuleContextInterface $context
+    ) {
+        if (version_compare($context->getVersion(), "1.0.0", "<")) {
+            //Your upgrade script
+        }
+    }
+}
+
---
+++ registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Ducpa23_FAQ',
+    __DIR__
+);
